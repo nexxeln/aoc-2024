3600ff8fac4ac4c6e39e73fa3e3c661c2bd575b5
Shoubhit Dash <shoubhit2005@gmail.com>
2024-12-11 10:38:31 +0530

    day 11

---
+++ day-11/2.py
@@ -0,0 +1,26 @@
+def trans(n):
+    if n == 0:
+        return [1]
+    s = str(n)
+    if len(s) % 2 == 0:
+        mid = len(s) // 2
+        return [int(s[:mid]), int(s[mid:])]
+    return [n * 2024]
+
+
+def blink(counts):
+    new = {}
+    for s, c in counts.items():
+        for ns in trans(s):
+            new[ns] = new.get(ns, 0) + c
+    return new
+
+
+c = {}
+for n in map(int, next(open(0)).split()):
+    c[n] = c.get(n, 0) + 1
+
+for _ in range(75):
+    c = blink(c)
+
+print(sum(c.values()))
