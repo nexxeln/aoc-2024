d55b3900285dac17597f1c2f3cabb7a456de3eba
Shoubhit Dash <shoubhit2005@gmail.com>
2024-12-28 15:25:35 +0530

    day 2

---
+++ haskell/day-02/Part1.hs
@@ -0,0 +1,28 @@
+import Data.List (nub)
+
+pairs :: [a] -> [(a, a)]
+pairs xs = zip xs (tail xs)
+
+differences :: [Int] -> [Int]
+differences xs = [b - a | (a, b) <- pairs xs]
+
+allUnique :: [Int] -> Bool
+allUnique xs = length (nub xs) == length xs
+
+isMonotonic :: [Int] -> Bool
+isMonotonic [] = True
+isMonotonic xs = all (> 0) xs || all (< 0) xs
+
+withinBounds :: [Int] -> Bool
+withinBounds = all (\x -> abs x >= 1 && abs x <= 3)
+
+isSafe :: [Int] -> Bool
+isSafe xs = allUnique xs 
+         && (let diffs = differences xs in 
+             isMonotonic diffs && withinBounds diffs)
+
+main :: IO ()
+main = do
+    contents <- readFile "input.txt"
+    let sequences = [[read x | x <- words line] | line <- lines contents]
+    print $ sum [1 | xs <- sequences, isSafe xs] 
